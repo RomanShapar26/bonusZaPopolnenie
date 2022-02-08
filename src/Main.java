e36c26009edd53b3852d9266ffbbbb0e6a76c0f3
Роман Шапарь <romanshaparution@gmail.com>
2022-02-08 20:26:17 +0300

    midif

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Входные данные
-        // На момент пополнения у клиента на счету 100 рублей
-
-        int amount = 1500;
-        boolean isRegistered = true;
-
-        // Логика
+        int amount = 1350;
 
         int percent;
         if (amount >= 1000) {
-            percent = 1;    // за каждые 100 рублей
+            percent = 1;
         } else {
             percent = 0;
         }
-
         int bonus = amount / 100 * percent;
-
-        if (amount > 1000) {
-            bonus = amount / 100 * 1;
-        }
-
         int generalBalance = 100 + amount + bonus;
-
-
         System.out.println("Бонус за пополнение:" + bonus);
         System.out.println("Бонус за пополнение:" + generalBalance);
 
